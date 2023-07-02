377456f377aca9820875a85a0c747f575bc6b121
Alexander Afanasiev <alexx.af@gmail.com>
2023-07-02 22:55:31 +0200

    Fix stuff on recipe_7_3_i

--- ch07/recipe_7_3_i/src/main/java/com/apress/spring6recipes/bookshop/reactive/Main.java
+++ ch07/recipe_7_3_i/src/main/java/com/apress/spring6recipes/bookshop/reactive/Main.java
@@ -1,19 +1,19 @@
 package com.apress.spring6recipes.bookshop.reactive;
 
-import com.apress.spring6recipes.bookshop.config.BookstoreConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
   public static void main(String[] args) throws Throwable {
-    var cfg = BookstoreConfiguration.class;
+    var cfg = ReactiveBookstoreConfiguration.class;
     try (var context = new AnnotationConfigApplicationContext(cfg)) {
       var bookShop = context.getBean(BookShop.class);
       var counter = new CountDownLatch(1);
       bookShop.purchase("0001", "user1").subscribe((x) -> counter.countDown());
-      counter.await();
+      counter.await(5, TimeUnit.SECONDS);
     }
   }
 }
